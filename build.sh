d618366d8008a07b332e30abb66f40a7bd8bc31a
Blizzard-cyber <369476975@qq.com>
2025-04-29 02:23:16 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -x
 
 SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_DIR=${BUILD_DIR:-./build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
 CXX=${CXX:-g++}
@@ -19,6 +19,18 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
            $SOURCE_DIR \
   && make $*
 
+# 如果执行了 install，就把生成的静态库和头文件拷贝到 test 目录下
+if [ "$1" = "install" ]; then
+  echo "Copying libs and headers to test/"
+  mkdir -p $SOURCE_DIR/test/lib
+  mkdir -p $SOURCE_DIR/test/include
+  # 只拷贝 muduo_net 和 muduo_base 两个静态库
+  cp -v "$INSTALL_DIR/lib/libmuduo_net.a"   "$SOURCE_DIR/test/lib/"
+  cp -v "$INSTALL_DIR/lib/libmuduo_base.a"  "$SOURCE_DIR/test/lib/"
+
+  # 头文件保持全部拷贝
+  cp -rv "$INSTALL_DIR/include/"*           "$SOURCE_DIR/test/include/"
+fi
 # Use the following command to run all the unit tests
 # at the dir $BUILD_DIR/$BUILD_TYPE :
 # CTEST_OUTPUT_ON_FAILURE=TRUE make test
--- test/FileTest.cc
+++ test/FileTest.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <muduo/net/EventLoop.h>
-#include "File.h"
+#include <muduo/net/File.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -12,8 +12,6 @@ protected:
   std::string filename_;
 
   void SetUp() override {
-    // mkstemp 会在 /tmp 下生成 testfileXXXXXX，
-    // 并打开文件返回 fd
     char tmpl[] = "/tmp/testfileXXXXXX";
     int fd = ::mkstemp(tmpl);
     ASSERT_GE(fd, 0);
@@ -26,26 +24,90 @@ protected:
   }
 };
 
-TEST_F(FileTest, WriteThenRead) {
-  File file(&loop_, filename_, O_RDWR);
-
-  // 1. 发起一次异步写
-  std::string s = "hello muduo";
-  file.write(s);
-
-  // 运行 loop，等写事件完成
+TEST_F(FileTest, WriteThenReadWithNewHandle) {
+  // 1. 异步写
+  File fileW(&loop_, filename_, O_RDWR);
+  std::string toWrite = "hello muduo";
+  fileW.write(toWrite);
   loop_.runAfter(0.1, [&](){ loop_.quit(); });
   loop_.loop();
 
-  // 2. 设置读回调，触发读
+  // 2. 新建一个只读 File 实例，偏移从头开始
+  File fileR(&loop_, filename_, O_RDONLY);
   std::string out;
-  file.setReadCallback([&](const std::string& buf){
+  fileR.setReadCallback([&](const std::string& buf){
     out = buf;
+    loop_.quit();
   });
-  file.read();
-
-  loop_.runAfter(0.1, [&](){ loop_.quit(); });
+  fileR.read();
+  // loop_.runAfter(0.1, [&](){ loop_.quit(); });
   loop_.loop();
 
-  EXPECT_EQ(out, s);
+  EXPECT_EQ(out, toWrite);
+}
+
+
+TEST_F(FileTest, ReadEmptyFile) {
+  // 打开一个新文件，未写入任何字节，直接读应得到空字符串
+  File fileR(&loop_, filename_, O_RDONLY);
+  std::string out;
+  fileR.setReadCallback([&](const std::string& buf) { out = buf; loop_.quit(); });
+  fileR.read();
+  loop_.runAfter(0.05, [&](){ loop_.quit(); });
+  loop_.loop();
+  EXPECT_EQ(out, "");
+}
+
+
+TEST_F(FileTest, MultipleWritesSingleRead) {
+  File fileW(&loop_, filename_, O_RDWR);
+
+  // 第一次写，然后跑 loop
+  fileW.write("hello ");
+  loop_.runAfter(0.05, [&](){ loop_.quit(); });
+  loop_.loop();
+
+  // 第二次写，再跑 loop
+  fileW.write("world");
+  loop_.runAfter(0.05, [&](){ loop_.quit(); });
+  loop_.loop();
+
+  // 读取
+  File fileR(&loop_, filename_, O_RDONLY);
+  std::string out;
+  fileR.setReadCallback([&](const std::string& buf){
+    out = buf; loop_.quit();
+  });
+  fileR.read();
+  loop_.runAfter(0.05, [&](){ loop_.quit(); });
+  loop_.loop();
+
+  EXPECT_EQ(out, "hello world");
+}
+
+TEST_F(FileTest, WriteAfterClose) {
+  File fileW(&loop_, filename_, O_RDWR);
+  fileW.close();
+
+  // 再次写不应抛异常，也不应触发写操作
+  EXPECT_NO_THROW(fileW.write("data"));
+
+  // 验证读不到任何数据
+  File fileR(&loop_, filename_, O_RDONLY);
+  std::string out;
+  fileR.setReadCallback([&](const std::string&){ out = "got"; loop_.quit(); });
+  fileR.read();
+  loop_.runAfter(0.05, [&](){ loop_.quit(); });
+  loop_.loop();
+
+  EXPECT_EQ(out, "");
+}
+
+TEST_F(FileTest, OpenNonExistFileForRead) {
+  std::string bad = filename_ + "_noexist";
+  // 放宽死亡测试匹配规则，匹配任意终止
+  EXPECT_DEATH(
+    { File f(&loop_, bad, O_RDONLY); },
+    ".*"
+  );
 }
