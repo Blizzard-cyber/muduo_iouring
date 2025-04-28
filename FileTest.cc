de0acb7b52ec04e3510b20110285172003ffeaba
Blizzard-cyber <369476975@qq.com>
2025-04-28 23:25:20 +0800

    init project without muduo source code

---
+++ FileTest.cc
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <muduo/net/EventLoop.h>
+
+#include "./lib/File.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+using namespace muduo::net;
+
+class FileTest : public ::testing::Test {
+protected:
+  EventLoop loop_;
+  std::string filename_;
+
+  void SetUp() override {
+    
+    char tmpl[] = "testfile.txt";
+    int fd = ::mkstemp(tmpl);
+    ASSERT_GE(fd, 0);
+    ::close(fd);
+    filename_ = tmpl;
+  }
+
+  void TearDown() override {
+    ::unlink(filename_.c_str());
+  }
+};
+
+TEST_F(FileTest, WriteThenRead) {
+  File file(&loop_, filename_, O_RDWR);
+
+  // 1. 发起一次异步写
+  std::string s = "hello muduo";
+  file.write(s);
+
+  // 运行 loop，等写事件完成
+  loop_.runAfter(0.1, [&](){ loop_.quit(); });
+  loop_.loop();
+
+  // 2. 设置读回调，触发读
+  std::string out;
+  file.setReadCallback([&](const std::string& buf){
+    out = buf;
+  });
+  file.read();
+
+  loop_.runAfter(0.1, [&](){ loop_.quit(); });
+  loop_.loop();
+
+  EXPECT_EQ(out, s);
+}
---
+++ lib/poller/iouringPoller.h
@@ -0,0 +1,43 @@
+// IoUringPoller.h
+#ifndef MUDUO_NET_POLLER_IOURING_POLLER_H
+#define MUDUO_NET_POLLER_IOURING_POLLER_H
+
+#include "../Poller.h"
+
+#include <liburing.h>
+#include <vector>
+
+namespace muduo
+{
+    namespace net
+    {
+        class IoUringPoller : public Poller
+        {
+        public:
+            IoUringPoller(EventLoop *loop);
+            ~IoUringPoller() override;
+
+            Timestamp poll(int timeoutMs, ChannelList *activeChannels) override; // 返回触发事件的所有fd对应的channel(epoll_wait)
+            void updateChannel(Channel *channel) override;                       // 更新channel的事件(epoll_ctl add/mod)
+            void removeChannel(Channel *channel) override;                       // 删除channel的事件(epoll_ctl del)       
+
+        private:
+            void fillActiveChannels(int numEvents, ChannelList *activeChannels) const; // 填充Channel中的激活事件
+
+            struct io_uring ring_; // io-uring核心变量
+
+            std::vector<struct io_uring_cqe *> events_; // 用于在一次poll()中存储从cqe中获取的所有结果
+        };
+
+        struct PollerInfo
+        {
+            Channel *channel;
+            uint32_t events; // 事件类型
+        };
+
+        
+
+    } // namespace net
+} // namespace muduo
+
+#endif // MUDUO_NET_POLLER_IOURING_POLLER_H
